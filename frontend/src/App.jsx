5b7becfbc6b57ede73edf2729c755f9fc8d6f586
Sivram Pradhan <programmersiv21@gmail.com>
2024-12-07 14:08:01 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/patient/me",
+          "https://meditrack-zbcm.onrender.com/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
